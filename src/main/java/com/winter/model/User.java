fe6164957d13c6dc222d60ef3ee889665d8caf11
ddxx6317 <ddxx5317@163.com>
2020-06-03 22:24:59 +0800

    springboot Interceptor

--- src/main/java/com/winter/model/User.java
+++ src/main/java/com/winter/model/User.java
@@ -3,8 +3,9 @@ package com.winter.model;
 import com.alibaba.fastjson.JSONObject;
 import com.winter.enums.SexEnum;
 import com.winter.utils.Signature;
+import lombok.Data;
 
-
+@Data
 public class User {
     private Integer userId;
 
@@ -18,63 +19,5 @@ public class User {
     
     private Signature signature;   
     
-    private SexEnum sex;   
-    
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName == null ? null : userName.trim();
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password == null ? null : password.trim();
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone == null ? null : phone.trim();
-    }
-
-	public JSONObject getConfig() {
-		return config;
-	}
-
-	public void setConfig(JSONObject config) {
-		this.config = config;
-	}
-
-	public Signature getSignature() {
-		return signature;
-	}
-
-	public void setSignature(Signature signature) {
-		this.signature = signature;
-	}
-
-	public SexEnum getSex() {
-		return sex;
-	}
-
-	public void setSex(SexEnum sex) {
-		this.sex = sex;
-	}
-
+    private SexEnum sex;
 }
